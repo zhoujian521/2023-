0cbb3efd7f491149340724fe43c4e6043d5a1785
zhoujj-f <zhoujj-f@glodon.com>
2023-01-31 09:52:19 +0800

    测试

--- typeOf.js
+++ typeOf.js
@@ -1,18 +1,22 @@
-function typeOf(val) {
-    return Object.prototype.toString().call(val).slice(8, -1).toLowerCase()
-}
+// function typeOf(val) {
+//     return Object.prototype.toString().call(val).slice(8, -1).toLowerCase()
+// }
 
-function foo() {
-    console.log('====================================');
-    console.log(this.name) //this.name 
-}
+// function foo() {
+//     console.log('====================================');
+//     console.log(this.name) //this.name 
+// }
 
-function bar() {
-    console.log('====================================');
-    var name = 'a' //私有变量
-    foo()
-}
-var name = 'b'
-bar()
+// function bar() {
+//     console.log('====================================');
+//     var name = 'a' //私有变量
+//     foo()
+// }
+// var name = 'b'
+// bar()
+
+console.log('====================================');
+console.log();
+console.log('====================================');
 
 //打印值  b
